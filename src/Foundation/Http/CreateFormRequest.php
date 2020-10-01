cfe5df34d196c65485150f7ceb66149d1ef57691
tarkhov <agtuks@gmail.com>
2020-10-02 02:08:14 +0300

    Fix Create/Delete FormRequest and udate LICENSE years

--- src/Foundation/Http/CreateFormRequest.php
+++ src/Foundation/Http/CreateFormRequest.php
@@ -6,7 +6,7 @@ abstract class CreateFormRequest extends ModelFormRequest
     public function rules()
     {
         $rules = parent::rules();
-        if (empty($this->fields)) {
+        if (empty($this->fields) && isset($rules[$this->primaryKey])) {
             unset($rules[$this->primaryKey]);
         }
         return $rules;
